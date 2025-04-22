e9fd8ccdec3735ebdee0c00360a953d7c8e8316f
kakiuchi-shigenao <kakiuchi-shigenao@wiznet.co.jp>
2025-04-22 18:53:09 +0900

    feat: メソッドをfinalに変更し、StringValueクラスのインスタンス生成を制限

--- src/String/StringValue.php
+++ src/String/StringValue.php
@@ -34,7 +34,7 @@ abstract readonly class StringValue implements IStringValue
     /**
      * 信頼できるプリミティブ値からインスタンスを生成する
      */
-    public static function from(string $value): static
+    final public static function from(string $value): static
     {
         return new static($value);
     }
@@ -43,7 +43,7 @@ abstract readonly class StringValue implements IStringValue
      * 信頼できるプリミティブ値からインスタンスを生成する（Null許容）
      * @return Option<static>
      */
-    public static function fromNullable(?string $value): Option
+    final public static function fromNullable(?string $value): Option
     {
         if ($value === null) {
             return Option\none();
@@ -56,19 +56,19 @@ abstract readonly class StringValue implements IStringValue
      * 信頼できないプリミティブ値からインスタンスを生成する
      * @return Result<static,StringValueError>
      */
-    public static function tryFrom(string $value): Result
+    final public static function tryFrom(string $value): Result
     {
         return static::isValid($value)
             ->andThen(static fn () => self::isLengthValid($value))
             ->andThen(static fn () => self::isRegexValid($value))
-            ->andThen(static fn () => Result\ok(new static($value)));
+            ->andThen(static fn () => Result\ok(static::from($value)));
     }
 
     /**
      * 信頼できないプリミティブ値からインスタンスを生成する（Null許容）
      * @return Result<Option<static>,StringValueError>
      */
-    public static function tryFromNullable(?string $value): Result
+    final public static function tryFromNullable(?string $value): Result
     {
         if ($value === null) {
             // @phpstan-ignore-next-line
@@ -83,7 +83,7 @@ abstract readonly class StringValue implements IStringValue
      * 有効な文字列長かどうか
      * @return Result<bool,StringValueError>
      */
-    private static function isLengthValid(string $value): Result
+    final public static function isLengthValid(string $value): Result
     {
         $value_length = mb_strlen($value, 'UTF-8');
         $min_length = static::minLength() > self::MIN_LENGTH ? static::minLength() : self::MIN_LENGTH;
@@ -105,7 +105,7 @@ abstract readonly class StringValue implements IStringValue
      * 有効な正規表現かどうか
      * @return Result<bool,StringValueError>
      */
-    private static function isRegexValid(string $value): Result
+    final public static function isRegexValid(string $value): Result
     {
         $regex = static::regex();
 
